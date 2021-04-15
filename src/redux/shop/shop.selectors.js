2d9dcee36e8dbeb9f7fe5ad72388f015d904b9a1
Johann Alberts <alberts.johann@gmail.com>
2021-04-15 22:21:54 +0100

    Create Collection page and normalise data

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from "reselect";
+import memoize from "lodash.memoize";
 
 const selectShop = (state) => state.shop;
 
@@ -6,3 +7,9 @@ export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  );
